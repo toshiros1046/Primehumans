be254234f2308d7be2d13e7920bcdce880f814fd
toshiros1046 <toshiros1046@gmail.com>
2016-06-18 12:54:37 +0900

    Initial Commit

---
+++ Primehuman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Primehuman
+//
+//  Created by 佐藤 駿郎 on 2016/06/18.
+//  Copyright (c) 2016年 佐藤 駿郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
