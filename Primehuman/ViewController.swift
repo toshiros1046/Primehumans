a79250f067787f2f39a4007cbd44def582b64ba7
toshiros1046 <toshiros1046@gmail.com>
2016-06-18 13:36:06 +0900

    commit
    コミット

--- Primehuman/ViewController.swift
+++ Primehuman/ViewController.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func tapButton(sender: AnyObject) {
+        
+        let coordinate = CLLocationCoordinate2DMake(37.331652997806785, -122.03072304117417)
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        
+        let region = MKCoordinateRegionMake(coordinate, span)
+        mapView.setRegion(region, animated:true)
+        
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
